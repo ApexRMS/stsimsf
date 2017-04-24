5de9de08b626a2c739f0ab014a96dad286ca6cc5
alex <alexe_webmail@yahoo.com>
2017-04-24 09:11:25 -0400

    General cleanup of file header blocks

--- Shared/Constants.vb
+++ Shared/Constants.vb
@@ -1,9 +1,9 @@
-﻿'************************************************************************************
-' STSimStockFlow: A .NET library for simulating stocks and flows
+﻿'*********************************************************************************************
+' STSimStockFlow: A SyncroSim Module for the ST-Sim Stocks and Flows Add-In.
 '
 ' Copyright © 2007-2017 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 '
-'************************************************************************************
+'*********************************************************************************************
 
 Module Constants
 
