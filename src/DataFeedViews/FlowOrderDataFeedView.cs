876d5c682e9f7e91639846fe4851e3d780efd080
alex <alexe_webmail@yahoo.com>
2019-02-01 16:49:45 -0800

    Update file header blocks to match readme.md.

--- src/DataFeedViews/FlowOrderDataFeedView.cs
+++ src/DataFeedViews/FlowOrderDataFeedView.cs
@@ -1,4 +1,4 @@
-﻿// STSimStockFlow: A SyncroSim Package for the ST-Sim Stocks and Flows Add-In.
+﻿// stsim-stockflow: SyncroSim Add-On Package (to stsim) for integrating stocks and flows into state-and-transition simulation models in ST-Sim.
 // Copyright © 2007-2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 
 using System.Reflection;
